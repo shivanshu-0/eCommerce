0ab1b5b6bb777a29d9fc8ec52e19ca1b2ab091ad
shivanshu-0 <shivanshutripathi1420@gmail.com>
2020-05-17 21:59:07 +0530

    Activity for admin where he verifies ordered products

--- app/src/main/java/com/example/ecommerce/AdminCategoryActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminCategoryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView tShirts, sportsTShirts, femaleDresses, sweathers;
     private ImageView glasses,hatsCaps,walletBagsPurses,shoes;
     private ImageView headPhonesHandFree,Laptops,watches,mobilePhones;
+
+    private Button logoutBtn, checkOrdersBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,27 @@ public class AdminCategoryActivity extends AppCompatActivity {
         Laptops=(ImageView) findViewById(R.id.laptop_pc);
         watches=(ImageView) findViewById(R.id.watches);
         mobilePhones=(ImageView) findViewById(R.id.mobilephones);
+
+        logoutBtn=(Button) findViewById(R.id.admin_logout_btn);
+        checkOrdersBtn=(Button) findViewById(R.id.check_order_btn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        checkOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
 
         tShirts.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/ecommerce/AdminNewOrdersActivity.java
@@ -0,0 +1,86 @@
+package com.example.ecommerce;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.ecommerce.Model.AdminOrders;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AdminNewOrdersActivity extends AppCompatActivity {
+
+
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_new_orders);
+
+        ordersRef= FirebaseDatabase.getInstance().getReference().child("Orders");
+        ordersList=findViewById(R.id.orders_list);
+        ordersList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<AdminOrders> options=
+                new FirebaseRecyclerOptions.Builder<AdminOrders>()
+                .setQuery(ordersRef,AdminOrders.class)
+                .build();
+
+        FirebaseRecyclerAdapter<AdminOrders,AdminOrdersViewHolder> adapter=
+                new FirebaseRecyclerAdapter<AdminOrders, AdminOrdersViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull AdminOrdersViewHolder adminOrdersViewHolder, int i, @NonNull AdminOrders adminOrders) {
+                        adminOrdersViewHolder.userName.setText("Name: "+adminOrders.getName());
+                        adminOrdersViewHolder.userPhone.setText("Phone: "+adminOrders.getPhone());
+                        adminOrdersViewHolder.userTotalPrice.setText("Total Amount: "+adminOrders.getTotalAmount());
+                        adminOrdersViewHolder.userDateTime.setText("Order at: "+adminOrders.getDate()+"  "+adminOrders.getTime());
+                        adminOrdersViewHolder.userShippingAddress.setText("Shipping Address: "+adminOrders.getAddress()+", "+adminOrders.getCity());
+                    }
+
+                    @NonNull
+                    @Override
+                    public AdminOrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.orders_layout,parent,false);
+                        return new AdminOrdersViewHolder(view);
+                    }
+                };
+        ordersList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class AdminOrdersViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView userName, userPhone, userTotalPrice, userDateTime, userShippingAddress;
+
+        public Button showOrdersBtn;
+        public AdminOrdersViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName=itemView.findViewById(R.id.order_user_name);
+            userPhone=itemView.findViewById(R.id.order_phone_number);
+            userTotalPrice=itemView.findViewById(R.id.order_total_price);
+            userDateTime=itemView.findViewById(R.id.order_date_time);
+            userShippingAddress=itemView.findViewById(R.id.order_address_city);
+
+            showOrdersBtn=itemView.findViewById(R.id.show_all_products_btn);
+
+        }
+    }
+}
