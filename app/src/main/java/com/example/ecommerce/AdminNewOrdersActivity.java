17271bfbb93884fcbebe08252554a3783dabaad6
shivanshu-0 <shivanshutripathi1420@gmail.com>
2020-05-18 13:01:09 +0530

    Feature To Search Product in home activity of user

--- app/src/main/java/com/example/ecommerce/AdminNewOrdersActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminNewOrdersActivity.java
@@ -1,10 +1,12 @@
 package com.example.ecommerce;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -64,6 +66,33 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
                                 startActivity(intent);
                             }
                         });
+                        adminOrdersViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                CharSequence options[]=new CharSequence[]{
+                                    "Yes",
+                                    "No"
+                                };
+
+                                AlertDialog.Builder builder=new AlertDialog.Builder(AdminNewOrdersActivity.this);
+                                builder.setTitle("Have You Shipped This Order Products?");
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if(which==0){
+                                            String uID=getRef(i).getKey();
+
+                                            removeOrder(uID);
+                                        }
+                                        else{
+                                            finish();
+                                        }
+                                    }
+                                });
+
+                                builder.show();
+                            }
+                        });
 
                     }
 
@@ -95,5 +124,9 @@ public class AdminNewOrdersActivity extends AppCompatActivity {
             showOrdersBtn=itemView.findViewById(R.id.show_all_products_btn);
 
         }
+    }
+
+    private void removeOrder(String uID) {
+        ordersRef.child(uID).removeValue();
     }
 }
