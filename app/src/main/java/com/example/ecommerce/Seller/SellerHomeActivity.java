02bc3e210bcddd4b9d34612f5515108d2021d24d
shivanshu-0 <shivanshutripathi1420@gmail.com>
2020-05-19 20:38:43 +0530

    On Seller Home Activity-seller can see all his approved and not approved products and also delete products.

--- app/src/main/java/com/example/ecommerce/Seller/SellerHomeActivity.java
+++ app/src/main/java/com/example/ecommerce/Seller/SellerHomeActivity.java
@@ -1,25 +1,48 @@
 package com.example.ecommerce.Seller;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.ecommerce.Admin.AdminCheckNewProductsActivity;
 import com.example.ecommerce.Buyer.MainActivity;
+import com.example.ecommerce.Model.Products;
 import com.example.ecommerce.R;
+import com.example.ecommerce.ViewHolder.ItemViewHolder;
+import com.example.ecommerce.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SellerHomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference unverifiedProductsRef;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemReselectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,7 +50,8 @@ public class SellerHomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()){
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    Intent intentHome=new Intent(SellerHomeActivity.this, SellerHomeActivity.class);
+                    startActivity(intentHome);
                     return true;
 
 
@@ -58,13 +82,84 @@ public class SellerHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_seller_home);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
         mTextMessage=findViewById(R.id.message);
-
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemReselectedListener);
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        //NavigationUI.setupWithNavController(navView, navController);
+
+        unverifiedProductsRef= FirebaseDatabase.getInstance().getReference().child("Products");
+        recyclerView=findViewById(R.id.seller_home_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options=
+                new FirebaseRecyclerOptions.Builder<Products>()
+                        .setQuery(unverifiedProductsRef.orderByChild("sid")
+                                .equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid()),Products.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Products, ItemViewHolder> adapter=
+                new FirebaseRecyclerAdapter<Products, ItemViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ItemViewHolder productViewHolder, int i, @NonNull final Products products) {
+                        productViewHolder .textProductName.setText(products.getPname());
+                        productViewHolder .textProductDescription.setText(products.getDescription());
+                        productViewHolder .textProductStatus.setText("State: " +products.getProductState());
+                        productViewHolder .textProductPrice.setText("Price: "+products.getPrice() + "$");
+                        Picasso.get().load(products.getImage()).into(productViewHolder.imageView);
+
+                        productViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                final String productId=products.getPid();
+                                CharSequence options[]=new CharSequence[]
+                                        {
+                                                "Yes",
+                                                "No"
+                                        };
+                                AlertDialog.Builder builder=new AlertDialog.Builder(SellerHomeActivity.this);
+                                builder.setTitle("Do You Want To Delete This Product");
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if(which==0){
+                                            deleteProduct(productId);
+                                        }
+                                        if(which==1){
+
+                                        }
+                                    }
+                                });
+                                builder.show();
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_seller_item_view,parent,false);
+                        ItemViewHolder holder=new ItemViewHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    private void deleteProduct(String productId) {
+        unverifiedProductsRef.child(productId)
+                .removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        Toast.makeText(SellerHomeActivity.this, "This Product has been deleted successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
 }
---
+++ app/src/main/java/com/example/ecommerce/ViewHolder/ItemViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.ecommerce.ViewHolder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ecommerce.Interface.ItemClickListener;
+import com.example.ecommerce.R;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView textProductName, textProductDescription, textProductPrice,textProductStatus;
+    public ImageView imageView;
+    public ItemClickListener listner;
+
+
+
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        imageView=(ImageView) itemView.findViewById(R.id.product_seller_image);
+        textProductName=(TextView) itemView.findViewById(R.id.product_seller_name);
+        textProductDescription=(TextView) itemView.findViewById(R.id.product_seller_description);
+        textProductPrice=(TextView) itemView.findViewById(R.id.product_seller_price);
+        textProductStatus=(TextView) itemView.findViewById(R.id.product_status);
+    }
+
+    public void setItemClickListner(ItemClickListener listner){
+        this.listner=listner;
+    }
+
+    @Override
+    public void onClick(View v) {
+        listner.onClick(v,getAdapterPosition(),false);
+    }
+}
+
